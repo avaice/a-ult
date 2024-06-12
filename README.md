572ca325e0887ad0e9536226f94390c830433119
avaice <dev@cho-ice.xyz>
2024-06-13 06:20:55 +0900

    add document

---
+++ README.md
@@ -0,0 +1,17 @@
+# Auto Alt Generator
+
+Auto Alt Generator is a tool that generates alt text for images using a GPT-4o.
+
+Developed with Preact + Tailwind CSS + Vite ☆
+
+## Installation
+
+```bash
+bun install
+```
+
+## Usage
+
+```bash
+bun run dev
+```
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,14 @@ export const Header = () => {
     <header className="my-4">
       <h1 className="text-4xl">Auto Alt Generator</h1>
       <p>
-        OpenAI APIを使用して、自動で画像の説明を生成します。{" "}
+        <a
+          href="https://platform.openai.com/"
+          target="_blank"
+          className="text-blue-600 underline hover:text-blue-800"
+        >
+          OpenAI API
+        </a>
+        を使用して、自動で画像の説明を生成します。
         <a
           href="https://github.com/avaice/a-ult"
           target="_blank"
